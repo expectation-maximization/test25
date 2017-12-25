f527989aafa1c9ee41086713f3645ef357c787e2
xulikui <xulikui@sogou-inc.com>
2017-12-25 21:15:57 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: test.cpp
+    > Author: ma6174
+    > Mail: ma6174@163.com 
+    > Created Time: Mon Dec 25 20:39:42 2017
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+
+int main(int argc, char* argv[]){
+	return 0;
+}
